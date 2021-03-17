48a4716a42c8d3afbee564fa84b02e20ec563eed
jaqueline <jaquelinesa.82@gmail.com>
2021-03-17 18:50:43 -0300

    Pytest Fixtures Criado pacote dentro do libpytools/tests/tests_spam/test_enviador_de_email.py Criado pacote dentro do libpytools/spam/  - enviador_de_email.py  - test_usuario  - classe Sessao e Conexao para o arquivo db.py na pasta spam  - classe Usuario  para o arquivo modelo.py na pasta spam

---
+++ libpytools/tests/test_spam/test_enviador_de_email.py
@@ -0,0 +1,34 @@
+import pytest
+
+from libpytools.spam.enviador_de_email import Enviador, EmailInvalido
+
+
+def test_criar_enviador_de_email():
+    enviador=Enviador()
+    assert enviador is not None
+
+
+@pytest.mark.parametrize('remetente', ['jaquelinesa.82@gmail.com', 'foo@bar.com.br'])
+def test_remetente(remetente):
+    enviador = Enviador()
+    resultado = enviador.enviar(
+        remetente,
+        'ana@python.pro.br',
+        'Curso Python Pro',
+        'Primeira turma 2021'
+    )
+    assert remetente in resultado
+
+
+@pytest.mark.parametrize('remetente', ['', 'jaqueline'])
+def test_remetente_invalido(remetente):
+    enviador = Enviador()
+    with pytest.raises(EmailInvalido):
+        enviador.enviar(
+            remetente,
+            'jaqueline@python.pro.br',
+            'Curso Python Pro',
+            'Primeira turma 2021'
+        )
+
+
---
+++ libpytools/tests/test_spam/test_usuarios.py
@@ -0,0 +1,34 @@
+import pytest
+
+from libpytools.spam.db import Conexao
+from libpytools.spam.modelos import Usuario
+
+
+@pytest.fixture
+def conexao():
+    # Setup
+    conexao_obj = Conexao()
+    yield conexao_obj
+    # Tear Down
+    conexao_obj.fechar()
+
+
+@pytest.fixture
+def sessao(conexao):
+    sessao_obj = conexao.gerar_sessao()
+    yield sessao_obj
+    sessao_obj.roll_back()
+    sessao_obj.fechar()
+
+
+def test_salvar_usuario(sessao):
+    usuario = Usuario(nome='jaqueline')
+    sessao.salvar(usuario)
+    assert isinstance(usuario.id, int)
+
+
+def test_listar_usuarios(sessao):
+    usuarios = [Usuario(nome='Jaqueline'), Usuario(nome='Ana')]
+    for usuario in usuarios:
+        sessao.salvar(usuario)
+    assert usuarios == sessao.listar()
