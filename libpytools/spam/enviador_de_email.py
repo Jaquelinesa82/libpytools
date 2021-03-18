28fa3f5092676484e3761081b4f142a478081624
jaqueline <jaquelinesa.82@gmail.com>
2021-03-18 16:26:17 -0300

    Inserido teste de parametros de spam
    close #29

--- libpytools/spam/enviador_de_email.py
+++ libpytools/spam/enviador_de_email.py
@@ -1,11 +1,7 @@
 class Enviador:
-    def __init__(self):
-        self.qtd_email_envidos = 0
-
     def enviar(self, remetente, destinatario, assunto, corpo):
         if '@' not in remetente:
             raise EmailInvalido(f'Email de remetente e inválido: {remetente}')
-        self.qtd_email_envidos += 1
         return remetente
 
 
--- libpytools/tests/test_spam/test_envio_para_base_de_usuarios.py
+++ libpytools/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -4,13 +4,23 @@ from libpytools.spam.enviador_de_email import Enviador
 from libpytools.spam.main import EnviadorDeSpam
 from libpytools.spam.modelos import Usuario
 
+class EnviadorMock(Enviador):
+    def __init__(self):
+        super().__init__()
+        self.qtd_email_enviados = 0
+        self.parametros_de_envio = None
+
+    def enviar(self, remetente, destinatario, assunto, corpo):
+        self.parametros_de_envio = (remetente, destinatario, assunto, corpo)
+        self.qtd_email_enviados += 1
+
 
 @pytest.mark.parametrize(
     'usuarios',
     [
         [
             Usuario(nome='Jaqueline', email='jaquelinesa.82@gmail.com'),
-            Usuario(nome='Ana', email='jaquelinesa.82@gmail.com')
+            Usuario(nome='Ana', email='ana@gmail.com')
         ],
         [
             Usuario(nome='Jaqueline', email='jaquelinesa.82@gmail.com')
@@ -20,12 +30,28 @@ from libpytools.spam.modelos import Usuario
 def test_qtd_de_spam(sessao, usuarios):
     for usuario in usuarios:
         sessao.salvar(usuario)
-    enviador = Enviador
-    enviador = Enviador()
+    enviador = EnviadorMock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'jaquelinesa.82@gmail.com',
         'Curso Python Pro',
         'Confira os módulos'
     )
-    assert len(usuarios) == enviador.qtd_email_envidos
+    assert len(usuarios) == enviador.qtd_email_enviados
+
+def test_parametros_de_spam(sessao):
+    usuario = Usuario(nome='Jaqueline', email='jaquelinesa.82@gmail.com')
+    sessao.salvar(usuario)
+    enviador = EnviadorMock()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
+    enviador_de_spam.enviar_emails(
+        'ana@gmail.com',
+        'Curso Python Pro',
+        'Confira os módulos'
+    )
+    assert enviador.parametros_de_envio == (
+        'ana@gmail.com',
+        'jaquelinesa.82@gmail.com',
+        'Curso Python Pro',
+        'Confira os módulos'
+    )
